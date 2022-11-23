a16620b6f3ae2e7ffa6d134d306f0d08cd0a1e77
Alexander <mrreads@yandex.com>
2022-11-23 19:31:30 +0300

    block order

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log('init');
+let time = 60 * 30 - 1;
+
+setInterval(() => {
+    let element = document.querySelector('#timer');
+    let duration = time;
+    let minutes = parseInt(time / 60, 10);
+    let seconds = parseInt(time % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    element.textContent = minutes + ":" + seconds;
+
+    if (--time < 0)
+        time = duration;
+}, 1000);
